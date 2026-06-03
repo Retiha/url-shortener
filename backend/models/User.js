dab8c216a5c322d585bd931864ea06ef5d7b855e
Retiha <retiha.c@gmail.com>
2026-06-03 13:51:44 +0530

    Fix async pre save hook in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -27,7 +27,6 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.comparePassword = async function(candidatePassword) {
